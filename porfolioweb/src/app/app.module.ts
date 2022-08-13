dcdd7c94bb3c3c3f4eb1db6437d437ebf3adef0a
German Gimenez <grmngimnez@gmail.com>
2022-08-13 11:17:32 -0300

    barradenavegacion2

--- porfolioweb/src/app/app.module.ts
+++ porfolioweb/src/app/app.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BarradenavegacionComponent } from './compartido/barradenavegacion/barradenavegacion/barradenavegacion.component';
+import { BarradenavegacionComponent } from './compartido/barradenavegacion/barradenavegacion.component';
+import { PiedepaginaComponent } from './compartido/piedepagina/piedepagina.component';
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarradenavegacionComponent
+    BarradenavegacionComponent,
+    PiedepaginaComponent,
+    
+    
+    
   ],
   imports: [
     BrowserModule
