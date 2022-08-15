95c70806e91370643c3a2a637977f4b2dbd8b683
grmangimenez <grmangimenez@gmail.com>
2022-08-15 20:21:56 -0300

    primer servicio en acercad y banner con archivos json

--- porfolioweb/src/app/componentes/seccion-acercad/seccion-acercad.component.ts
+++ porfolioweb/src/app/componentes/seccion-acercad/seccion-acercad.component.ts
@@ -4,23 +4,21 @@ import { DatosporfoliowebService } from 'src/app/servicios/datosporfolioweb.serv
 @Component({
   selector: 'app-seccion-acercad',
   templateUrl: './seccion-acercad.component.html',
-  styleUrls: ['./seccion-acercad.component.css']
+  styleUrls: ['./seccion-acercad.component.css'],
 })
 export class SeccionAcercadComponent implements OnInit {
+  url = './assets/seccion-acercad/datos/acercad.json';
+  acercad = '';
 
-  url="./assets/seccion-acercad/datos/acercad.json";
-  acercad = ""
-
-  constructor(private datosporfolio:DatosporfoliowebService) { }
+  constructor(private datosporfolio: DatosporfoliowebService) {}
 
   ngOnInit(): void {
-    this.datosporfolio.obtenerdatos(this.url).subscribe(datos=>{
+    this.datosporfolio.obtenerdatos(this.url).subscribe((datos) => {
       this.acercad = datos.acercad;
-      console.log(this.acercad + " desde el servicio con parametros");
-
-
-    })
-    console.log("los datos estan en : ./assets/seccion-acercad/datos/acercade.json ");
+      console.log(this.acercad + ' desde el servicio con parametros');
+    });
+    console.log(
+      'los datos estan en : ./assets/seccion-acercad/datos/acercade.json '
+    );
   }
-
 }
--- porfolioweb/src/app/componentes/seccion-banner/seccion-banner.component.ts
+++ porfolioweb/src/app/componentes/seccion-banner/seccion-banner.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DatosporfoliowebService } from 'src/app/servicios/datosporfolioweb.service';
 
 @Component({
   selector: 'app-seccion-banner',
   templateUrl: './seccion-banner.component.html',
-  styleUrls: ['./seccion-banner.component.css']
+  styleUrls: ['./seccion-banner.component.css'],
 })
 export class SeccionBannerComponent implements OnInit {
+  url = "./assets/seccion-banner/datos/banner.json";
+  banner: any;
 
-  constructor() { }
+  constructor(private datosporfolio: DatosporfoliowebService) {}
 
   ngOnInit(): void {
+    this.datosporfolio.obtenerdatos(this.url).subscribe((datos) => {
+      this.banner = datos;
+      console.log(this.banner + 'desde el servicio con parametros');
+    });
+    console.log(
+      'los datos estan en : ./assets/seccion-banner/datos/banner.json '
+    );
   }
-
 }
